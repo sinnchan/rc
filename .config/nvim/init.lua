f7fa375d3adf9894484571cb2b738981a5165685
sinnchan <sinn67c3z42@icloud.com>
2023-06-29 14:29:55 +0900

    add config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,29 @@
+
+vim.opt.number = true
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+local packer = require('packer')
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  if packer_bootstrap then
+    packer.sync()
+  end
+end)
+
+
